9d8a651e1d88cc38984189529d84a961b242a4dd
Dennis Johnston <dennisjohnston1979@gmail.com>
2023-07-16 11:12:49 +0200

    Finished 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  return Math.floor(5 / 9 * (degrees - 32));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  return Math.floor(degrees * (9/5) + 32);
 };
 
 // Do not edit below this line
